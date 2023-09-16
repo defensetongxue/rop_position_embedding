514e891a8e31e0c41060f46b4fec2052298d89db
defense <1900013009@pku.edu.cn>
2023-09-16 12:15:07 +0800

    style

--- utils_/function_.py
+++ utils_/function_.py
@@ -2,23 +2,36 @@ import torch
 from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 
+def to_device(x, device):
+    if isinstance(x, tuple):
+        return tuple(to_device(xi, device) for xi in x)
+    elif isinstance(x,list):
+        return [to_device(xi,device) for xi in x]
+    else:
+        return x.to(device)
+
 def train_epoch(model, optimizer, train_loader, loss_function, device):
     model.train()
     running_loss = 0.0
 
-    for inputs, targets,meta in train_loader:
-        inputs = inputs.to(device)
-        targets = targets.to(device)
+    for inputs, targets, meta in train_loader:
+        # Moving inputs and targets to the correct device
+        inputs = to_device(inputs, device)
+        targets = to_device(targets, device)
 
         optimizer.zero_grad()
 
+        # Assuming your model returns a tuple of outputs
         outputs = model(inputs)
+
+        # Assuming your loss function can handle tuples of outputs and targets
         loss = loss_function(outputs, targets)
 
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
+    
     return running_loss / len(train_loader)
 
 def val_epoch(model, val_loader, loss_function, device):
@@ -27,15 +40,15 @@ def val_epoch(model, val_loader, loss_function, device):
 
     with torch.no_grad():
         for inputs, targets,meta in val_loader:
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+            inputs = to_device(inputs, device)
+            targets = to_device(targets, device)
 
             outputs = model(inputs)
             loss = loss_function(outputs, targets)
 
             running_loss += loss.item()
-    return running_loss / len(val_loader)
 
+    return running_loss / len(val_loader)
 
 def get_instance(module, class_name, *args, **kwargs):
     cls = getattr(module, class_name)
